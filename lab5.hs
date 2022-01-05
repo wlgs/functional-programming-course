d91f655a50372f13ede57676b3117d38b370f6ef
Mikołaj Wielgos <43356643+wlgs@users.noreply.github.com>
2022-01-05 11:29:08 +0100

    lab5 exam ended, full credit

--- lab5.hs
+++ lab5.hs
@@ -186,3 +186,17 @@ newtype MyTriple a = MyTriple (a,a,a) deriving Show
 
 
 -- trening do kartkowki:
+test :: p -> IO ()
+test s = getLine >>= \s -> return 3 >>= \n -> putStrLn $ show n ++ s
+
+data Tree a = Node a (Tree a) (Tree a)
+            | Leaf
+
+pathsSum:: Num a => Tree a -> [a]
+pathsSum Leaf = pure 0
+pathsSum (Node a lt rt) = concat $ ([(a +)] <*>) <$> (fmap pathsSum [lt,rt])
+
+paths:: Tree a -> [[a]]
+paths Leaf = pure []
+paths (Node a lt rt) = concat $ ([(a :)] <*>) <$> (fmap paths [lt,rt])
+
